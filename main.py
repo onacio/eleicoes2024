7901ea41d55dbc5e7f0d76d29daebcd5d546d970
Onácio Santana <nsonacio@hotmail.com>
2024-09-06 19:23:00 -0300

    atualização

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+# Ocupa a largura total da tela
 st.set_page_config(layout='wide')
 
 # Colunas que serão exibidas 
@@ -10,7 +11,7 @@ colunas = ['NM_VOTAVEL', 'NR_VOTAVEL', 'NR_SECAO', 'QT_VOTOS', 'NM_LOCAL_VOTACAO
 df = pd.read_csv('maragogipe.csv', delimiter=';', encoding='latin1')
 
 # Menu que seleciona o nome do votável na tela
-votavel = st.sidebar.selectbox('Votável', df['NM_VOTAVEL'].unique())
+votavel = st.sidebar.selectbox('Nome do candidato', df['NM_VOTAVEL'].unique())
 
 st.title('Eleição de 2020 - Maragogipe')
 
@@ -18,4 +19,4 @@ st.title('Eleição de 2020 - Maragogipe')
 df_filtrado = df[df['NM_VOTAVEL'] == votavel]
 
 # Exibe na tela o dataframe com os dados do votável que foi selecionado
-st.dataframe(df_filtrado[colunas])
+st.dataframe(df_filtrado[colunas], hide_index=True)
